f111147b45803407827d6c2796c63f9165c682a5
javaLDY <1039236687@qq.com>
2014-12-29 13:50:59 +0800

    啊啊

--- src/main/java/com/shinowit/web/IndexController.java
+++ src/main/java/com/shinowit/web/IndexController.java
@@ -2,6 +2,7 @@ package com.shinowit.web;
 
 import com.shinowit.dao.mapper.TmeMerchandisecinfoMapper;
 import com.shinowit.dao.mapper.TmeMerchandiseinfoMapper;
+import com.shinowit.dao.mapper.ToolsDao;
 import com.shinowit.entity.TmeMerchandisecinfo;
 import com.shinowit.entity.TmeMerchandisecinfoCriteria;
 import com.shinowit.entity.TmeMerchandiseinfo;
@@ -17,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2014/12/24.
@@ -33,9 +36,12 @@ public class IndexController {
     @Resource
     private TmeMerchandiseinfoMapper merchandisedao;
 
+    @Resource
+    private ToolsDao toolsDao;
+
     @RequestMapping("/merchandise")
     @ResponseBody
-    public List<TmeMerchandiseinfo> merchandiseselect(@RequestParam("merchanCid")String merCid,Model model){
+    public List<TmeMerchandiseinfo> merchandiseselect(@RequestParam("merchanCid")String merCid){
         List<TmeMerchandiseinfo> merchaninfolist = null;
         TmeMerchandiseinfoCriteria criteria1 = new TmeMerchandiseinfoCriteria();
         TmeMerchandiseinfoCriteria.Criteria tj2 = criteria1.createCriteria();
@@ -45,8 +51,22 @@ public class IndexController {
         return merchaninfolist;
     }
 
+//    @RequestMapping("/merchandisexinxi")
+//    public String merchandisexinxiselect(@RequestParam("merchanname")String merchanname,Model model){
+//        List<Map<String,Object>> merchaninfolist = null;
+//        try {
+//            byte[] bb = merchanname.getBytes("ISO-8859-1");
+//            merchanname=new String(bb,"UTF-8");
+//            merchaninfolist = toolsDao.selecmerchan(merchanname);
+//            model.addAttribute("merchandiseall",merchaninfolist);
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+//        return "/shinowit/index";
+//    }
+
     @RequestMapping("/index")
-    public String indexshow(@ModelAttribute("merchan")TmeMerchandiseinfo merchandiseinfo,Model model){
+    public String indexshow(@ModelAttribute("merchan")TmeMerchandiseinfo merchandiseinfo,Model model,@RequestParam(value = "pageindex",required = false) Integer pageIndex,@RequestParam(value = "merchanname",required = false)String merchanname){
         //商品类别查询
         List<TmeMerchandisecinfo> merchaninfoclist = new ArrayList<TmeMerchandisecinfo>();
         TmeMerchandisecinfoCriteria criteria = new TmeMerchandisecinfoCriteria();
@@ -61,12 +81,76 @@ public class IndexController {
         tj1.andSalestatusEqualTo(true);
         merchaninfolist = merchandisedao.selectByExample(criteria1);
         model.addAttribute("merchandise",merchaninfolist);
-        //商品信息主要展示栏
+        //商品分页展示
         TmeMerchandiseinfoCriteria criteria3 = new TmeMerchandiseinfoCriteria();
-        TmeMerchandiseinfoCriteria.Criteria tj3 = criteria1.createCriteria();
-        tj1.andMerchandiseidIsNotNull();
-        merchaninfolist = merchandisedao.selectByExample(criteria3);
+        TmeMerchandiseinfoCriteria.Criteria tj3 = criteria3.createCriteria();
+        tj3.andMerchandiseidIsNotNull();
+        int merchandisetoalnum = merchandisedao.selectByExample(criteria3).size();//商品信息的总数
+        int pagesumnum=0;
+        if(merchanname==null) {
+            if (pageIndex == null) {
+                pageIndex = 1;
+                int pageSize = 4;
+                pagesumnum = merchandisetoalnum / pageSize;
+                if (merchandisetoalnum % pageSize != 0) {
+                    pagesumnum = pagesumnum + 1;
+                }
+                criteria3.setPageIndex(pageIndex);
+                criteria3.setPageSize(pageSize);
+                merchaninfolist = merchandisedao.selectPage(criteria3);
+            } else {
+                int pageSize = 4;
+                pagesumnum = merchandisetoalnum / pageSize;
+                if (merchandisetoalnum % pageSize != 0) {
+                    pagesumnum = pagesumnum + 1;
+                }
+                criteria3.setPageIndex(pageIndex);
+                criteria3.setPageSize(pageSize);
+                merchaninfolist = merchandisedao.selectPage(criteria3);
+            }
+        }else{
+            List<Map<String,Object>> merchaninfolist1 = null;
+            try {
+                int pageSize=4;
+                byte[] bb = merchanname.getBytes("ISO-8859-1");
+                merchanname=new String(bb,"UTF-8");
+                if(pageIndex==null) {
+                    pageIndex = 1;
+                    merchaninfolist1 = toolsDao.pageselecmerchan(merchanname, pageSize, pageIndex);
+                    int merchandisetoalnum1 = toolsDao.selecmerchan(merchanname).size();
+                    int pagesumnum2 = merchandisetoalnum1 / pageSize;
+                    if (merchandisetoalnum % pageSize != 0) {
+                        pagesumnum2 = pagesumnum2 + 1;
+                    }
+                    model.addAttribute("pagesumnum", pagesumnum2);
+                    model.addAttribute("pageIndex", pageIndex);
+                    model.addAttribute("merchanname",merchanname);
+                    model.addAttribute("merchandiseall", merchaninfolist1);
+                    model.addAttribute("merchandisetoalnum", merchandisetoalnum1);
+                }else{
+                    merchaninfolist1 = toolsDao.pageselecmerchan(merchanname, pageSize, pageIndex);
+                    int merchandisetoalnum1 = toolsDao.selecmerchan(merchanname).size();
+                    int pagesumnum2 = merchandisetoalnum1 / pageSize;
+                    if (merchandisetoalnum % pageSize != 0) {
+                        pagesumnum2 = pagesumnum2 + 1;
+                    }
+                    model.addAttribute("pagesumnum", pagesumnum2);
+                    model.addAttribute("pageIndex", pageIndex);
+                    model.addAttribute("merchanname",merchanname);
+                    model.addAttribute("merchandiseall", merchaninfolist1);
+                    model.addAttribute("merchandisetoalnum", merchandisetoalnum1);
+                }
+                return "/index";
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
         model.addAttribute("merchandiseall",merchaninfolist);
+        model.addAttribute("merchandisetoalnum",merchandisetoalnum);
+        model.addAttribute("merchanname",merchanname);
+        model.addAttribute("pageIndex",pageIndex);
+        model.addAttribute("pagesumnum",pagesumnum);
         return "/index";
     }
 }
